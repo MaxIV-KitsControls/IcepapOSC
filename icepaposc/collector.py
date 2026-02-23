445237cd58b43c320822827f4b7307b8be2286f3
jlsmax <julio.lidon-simon@maxiv.lu.se>
2026-02-23 12:06:43 +0100

    260128 with new dtax and new collector
    Should work, non-generic attribs have to be adjusted still

--- icepaposc/collector.py
+++ icepaposc/collector.py
@@ -23,6 +23,21 @@ from icepap import IcePAPController
 from .channel import Channel
 import time
 
+# Imports for the IceDtaxDescriptor
+from ctypes import c_int32, c_int16
+import struct
+
+# General MODBUS includes
+from modbus_tk.exceptions import ModbusError
+import modbus_tk.defines as cst
+
+# TCP MODBUS includes
+from modbus_tk import modbus_tcp
+
+##from .dtax import dtax
+from dtax import dtax
+from types import MethodType
+
 SIGNAL_GETTER_MAP = [
     ("PosAxis", "_getter_pos_axis"),
     ("PosTgtenc", "_getter_pos_tgtenc"),
@@ -320,6 +335,280 @@ class IcePAPDescriptor:
         return float(self.icepap_system[addr].send_cmd("?isg ?inpval")[-2])
 
 
+class IceDtaxDescriptor(IcePAPDescriptor):
+    # Address = device_property(dtype=str, default_value="192.168.2.40")
+    # Address = device_property(dtype=str, default_value="10.0.108.171")
+    # Address = device_property(dtype=str, default_value="172.16.142.101")  # hippie
+    # Address = device_property(dtype=str, default_value="172.16.190.98")  #species
+    # Address = device_property(dtype=str, default_value="172.16.166.80")  # finest
+    # Address = device_property(dtype=str, default_value="172.16.182.96")  # flexpes
+    NotFlexpes = True
+    PortModbus = 502
+    DeviceID = 1
+    TimeOut = 5.0
+    d = dtax()
+    raw = False
+    speedRpsNotUnits = True
+    positionRevsNotUnits = False
+
+    # def __init__(self, host, port, timeout):
+    def __init__(self, icepap_controller, hostname):
+        host = hostname
+        self.host1 = host.split(";")[0]
+        self.host2 = host.split(";")[1]
+        # Get the icepap
+        self.icepap_system = icepap_controller
+        ##self.port = port
+        # since you need to connect to an icepap,
+        # use that in the machine where the moxa address is the same for all epus
+        if self.host1.lower() in [
+            "10.113.25",
+            "r1-d110710-cab40-ctl-ipap-03.maxiv.lu.se",
+            "r1-d110710-cab40-ctl-ipap-03",
+        ] or self.host2.lower() in [
+            "172.16.166.80",
+        ]:
+            self.NotFlexpes = False
+        else:
+            self.NotFlexpes = True
+        # Get the icepap
+        ## super().__init__(self.host1, self.port, timeout)
+        self.master = modbus_tcp.TcpMaster(host=self.host2, port=self.PortModbus)
+        self.master.set_verbose(False)
+        self.master.set_timeout(self.TimeOut)
+        for n, p in self.d.dtax_params.items():
+            if "getter" in p:
+                pname = p["getter"].split("_")[1]
+                fgetf = p["getter"]
+                param = n
+                methode_code = f'def {fgetf}(self, addr): return self.generic_read(addr, "{param}")'
+                exec(methode_code)
+                bound_method = MethodType(locals()[fgetf], self)
+                setattr(self, fgetf, bound_method)
+                self.sig_getters.update([(pname, bound_method)])
+                print(fgetf)
+        self.sig_getters.update(
+            [
+                ("voltagermotor", self.get_voltagermotor),
+                ("voltagelmotor", self.get_voltagelmotor),
+                ("voltagekmotor", self.get_voltagekmotor),
+            ]
+        )
+        self.sig_list = list(self.sig_getters.keys())
+
+    def getExtraSpeedFactorFromRPM(self, addr):
+        # rps is configured by default (not rpms). If units, change dtax
+        if not self.speedRpsNotUnits:
+            if not self.NotFlexpes:
+                return self.d.extraSpeedFactorFlexpes
+            elif int(addr) <= 4:
+                return self.d.extraSpeedFactorMMGap
+            else:
+                return self.d.extraSpeedFactorMMPhase
+        else:
+            return 1.0
+
+    def get_voltagermotor(self, addr):
+        par = "4.01"
+        self.digitax_get_parameter(addr, self.d.dtax_params[par])
+        result = self.digitaxDecodeFrame(
+            self.d.dtax_params[par], scale=self.d.dtax_params[par]["scale"]
+        )
+        if self.NotFlexpes and addr in [1, 2, 3, 4]:
+            resistance_phase = 48.2  # 5.17
+        else:
+            resistance_phase = 0.75
+        resistance_line2line = resistance_phase  # *2
+        if result is None:
+            return 0.0
+        return resistance_line2line * abs(result)
+
+    def get_voltagelmotor(self, addr):
+        par = "4.01"
+        self.digitax_get_parameter(addr, self.d.dtax_params[par])
+        curr = self.digitaxDecodeFrame(
+            self.d.dtax_params[par],
+            scale=self.d.dtax_params[par]["scale"],
+            force_not_raw=True,
+        )
+        par = "3.02"
+        self.digitax_get_parameter(addr, self.d.dtax_params[par])
+        rpms = self.digitaxDecodeFrame(
+            self.d.dtax_params[par],
+            scale=self.d.dtax_params[par]["scale"],
+            force_not_raw=True,
+        )
+        if self.NotFlexpes and addr in [1, 2, 3, 4]:
+            inductance = 99.2  # mh #5.24
+        else:
+            inductance = 19.700
+        inductancel2l = inductance  # * 2
+        polespairs = 3
+        result = (
+            2
+            * 3.14159
+            * inductancel2l
+            * 1e-3
+            * polespairs
+            * 60
+            * rpms
+            * (self.d.speedFactor)
+            * curr
+        )
+        if result is None:
+            return 0.0
+        return abs(result)
+
+    def get_voltagekmotor(self, addr):
+        par = "3.02"
+        self.digitax_get_parameter(addr, self.d.dtax_params[par])
+        rpms = self.digitaxDecodeFrame(
+            self.d.dtax_params[par],
+            scale=self.d.dtax_params[par]["scale"],
+            force_not_raw=True,
+        )
+        if self.NotFlexpes and addr in [1, 2, 3, 4]:
+            ke = 147  # 5.17 v/krpm
+        else:
+            ke = 98
+        if rpms is None:
+            return 0.0
+        return 1e-3 * ke * abs(rpms) * (1 / self.d.speedFactor)
+
+    def generic_read(self, addr, attr):
+        par = attr
+        if par.endswith("p") or par.endswith("g"):
+            if addr in [1, 2, 3, 4]:
+                par = par[:-1] + "g"
+        attr_name = self.d.dtax_params[par]["getter"].split("_")[1]
+        position_factor = 1.0
+        if attr_name.startswith("position"):
+            if not self.NotFlexpes:
+                position_factor = self.d.positionFactorFlexpes
+            elif addr in [1, 2, 3, 4]:
+                position_factor = self.d.positionFactorMMGap
+            else:
+                position_factor = self.d.positionFactorMMPhase
+        if attr_name.startswith("position") and "Rev" in attr_name:
+            if not self.NotFlexpes:
+                position_factor = self.d.positionRevFactorFlexpes
+            elif addr in [1, 2, 3, 4]:
+                position_factor = self.d.positionRevFactorMMGap
+            else:
+                position_factor = self.d.positionRevFactorMMPhase
+        if attr_name.startswith("speed"):
+            position_factor = self.getExtraSpeedFactorFromRPM(addr)
+        self.digitax_get_parameter(addr, self.d.dtax_params[par])
+        result = self.digitaxDecodeFrame(
+            self.d.dtax_params[par],
+            scale=self.d.dtax_params[par]["scale"],
+            extra_scale_factor=position_factor,
+        )
+        if result is None:
+            return 0.0
+        return result
+
+    def get_registerRange(self, addr, startRegistry, numberOfIndexes):
+        try:
+            self.registerTab = self.master.execute(
+                addr,
+                cst.READ_HOLDING_REGISTERS,
+                startRegistry,
+                numberOfIndexes,
+            )
+
+        except ModbusError as exc:
+            Except.re_throw_exception(
+                exc,
+                "modbusPLC.get_registerRange",
+                "modbusPLC.get_registerRange method failed.",
+            )
+        except Exception as exc:
+            if exc.errno == 8:
+                Except.re_throw_exception(
+                    exc,
+                    "modbusPLC.get_registerRange",
+                    "modbusPLC.get_registerRange method failed.",
+                )
+            else:
+                print("Nocommunication")
+
+    def get_digitax_address_from_menu_register(
+        self, menu, register, register_data_type
+    ):
+        REGISTER_TYPE_INT16_ADDR = 0
+        REGISTER_TYPE_INT32_ADDR = pow(14, 2)
+        REGISTER_TYPE_FLOAT_ADDR = pow(15, 2)
+        addr = None
+        if register_data_type == "int16":
+            addr = REGISTER_TYPE_INT16_ADDR + menu * 100 + register - 1
+        elif register_data_type == "int32":
+            addr = REGISTER_TYPE_INT32_ADDR + menu * 100 + register - 1
+        elif register_data_type == "float":
+            addr = REGISTER_TYPE_FLOAT_ADDR + menu * 100 + register - 1
+        else:
+            raise (NotImplementedError)
+        return addr
+
+    def get_digitax_number_of_modbus_registers(self, register_data_type):
+        nbytes = None
+        # print("gdnob:", register_data_type)
+        if register_data_type == "int16":
+            nbytes = 1
+        elif register_data_type == "int32":
+            nbytes = 2
+        elif register_data_type == "float":
+            nbytes = 2
+        else:
+            raise (NotImplementedError)
+        return nbytes
+
+    def digitax_get_parameter(self, addr, par):
+        self.get_registerRange(
+            addr,
+            self.get_digitax_address_from_menu_register(
+                par["menu"], par["register"], par["dtype"]
+            ),  # start reg
+            self.get_digitax_number_of_modbus_registers(
+                par["dtype"]
+            ),  # numberOfIndexes
+        )
+
+    def digitaxDecodeFrame(
+        self, par, scale=True, force_not_raw=False, extra_scale_factor=1.0
+    ):
+        try:
+            if not self.registerTab:
+                return None
+            if par["dtype"] == "int16":
+                val1 = self.registerTab[0]
+                if par["signed"]:
+                    val1 = c_int16(val1).value
+            elif par["dtype"] == "int32":
+                val1 = (self.registerTab[0] << 16) | self.registerTab[1]
+                if par["signed"]:
+                    val1 = c_int32(val1).value
+            elif par["dtype"] == "float":
+                val1 = (self.registerTab[0] << 16) | self.registerTab[1]
+                val1 = struct.unpack(">f", struct.pack(">I", val1))[0]
+            if False:
+                print(
+                    "dp:",
+                    par["menu"],
+                    par["register"],
+                    val1,
+                    val1 * par["factor"],
+                    par["default"],
+                )
+            if scale and (not self.raw or force_not_raw):
+                return val1 * par["factor"] * extra_scale_factor
+            else:
+                return 1.0 * val1
+        except Exception:
+            print("Decode Frame Error")
+            return None
+
+
 class Collector:
     """Feeds a subscriber with collected IcePAP signal data."""
 
@@ -353,18 +642,21 @@ class Collector:
         self.current_channel = 0
 
         try:
-            self.icepap_system = IcePAPDescriptor(icepap_controller, hostname)
+            ##self.icepap_system = IcePAPDescriptor(icepap_controller, hostname)
+            self.icepap_system = IceDtaxDescriptor(icepap_controller, hostname)
         except Exception as e:
             msg = (
                 "Failed to instantiate master controller.\nHost: "
-                "{}\nPort: {}\n{}".format(
-                    icepap_controller.host, icepap_controller.port, e
+                "{}\nPort: {}\nAlt hostname:{}\n{}".format(
+                    icepap_controller.host, icepap_controller.port, hostname, e
                 )
             )
             raise Exception(msg)
 
         if not self.icepap_system:
-            msg = "IcePAP system {} has no active drivers! " "Aborting.".format(host)
+            msg = "IcePAP system {} has no active drivers! " "Aborting.".format(
+                icepap_controller.host
+            )
             raise Exception(msg)
 
         self.sig_list = self.icepap_system.get_getters()
